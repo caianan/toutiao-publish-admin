36e00406e3852ead081884c33e25c58ad968f017
phelo <sanbei712@163.com>
2020-10-27 11:24:02 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+/* 项目的启动入口 */
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+// 创建 Vue 根实例
+// 把 router 配置到根实例中
+// 通过 render 方法把 App 根组件渲染到 #app入口节点
+new Vue({
+  router,
+  render: h => h(App)
+  // el:'#app'  等价于  @mount('#app')
+}).$mount('#app')
